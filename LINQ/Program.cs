50303367bfa4274f6bf02488cfdde49c733010ed
Allen Lewis <allenlewis32@gmail.com>
2023-04-19 11:59:17 +0530

    LINQ 4

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,15 +1,41 @@
-﻿namespace LINQ
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace LINQ
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			List<object> list = new() { 1, "one", 2, "two", 3, "three", 4, "four" };
-			var query = list.OfType<int>().ToList();
-			var query2 = (from a in list where a is string orderby a select a);
-			foreach (var item in query2)
+			List<int> numbers = new() { 5, 1, 4, 2, 3 };
+			numbers.Reverse();
+			var res = numbers.FindAll(x => x > 2);
+			foreach (var item in res) Console.WriteLine(item);
+
+			List<Student> students = new() { new("A", 14), new("B", 23), new("C", 41), new("D", 32) };
+
+			Console.WriteLine(students.Contains(new("A", 14), new Comp()));
+		}
+		class Comp : EqualityComparer<Student>
+		{
+			public override bool Equals(Student? x, Student? y)
 			{
-				Console.WriteLine(item);
+				if (x == null || y == null) return false;
+				return x.name == y.name && x.age == y.age;
+			}
+
+			public override int GetHashCode([DisallowNull] Student obj)
+			{
+				throw new NotImplementedException();
+			}
+		}
+		class Student
+		{
+			public string name;
+			public int age;
+			public Student(string name, int age)
+			{
+				this.name = name;
+				this.age = age;
 			}
 		}
 	}
